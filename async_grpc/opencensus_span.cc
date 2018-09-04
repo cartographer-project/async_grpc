1c471b4293ddbe893bf479ecdaa1c7615ac37a4d
Christoph Schütte <cschuet@google.com>
2018-09-04 14:57:10 +0200

    Wrap Opencensus tracing in interface. (#38)

---
+++ async_grpc/opencensus_span.cc
@@ -0,0 +1,44 @@
+/*
+ * Copyright 2018 The Cartographer Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "async_grpc/opencensus_span.h"
+
+#if BUILD_TRACING
+
+namespace async_grpc {
+
+std::unique_ptr<Span> OpencensusSpan::StartSpan(const std::string& name,
+                                                const OpencensusSpan* parent) {
+  return std::unique_ptr<OpencensusSpan>(new OpencensusSpan(name, parent));
+}
+
+std::unique_ptr<Span> OpencensusSpan::CreateChildSpan(const std::string& name) {
+  return std::unique_ptr<OpencensusSpan>(new OpencensusSpan(name, this));
+}
+
+void OpencensusSpan::SetStatus(const ::grpc::Status& status) {
+  span_.SetStatus((opencensus::trace::StatusCode)status.error_code());
+}
+
+void OpencensusSpan::End() { span_.End(); }
+
+OpencensusSpan::OpencensusSpan(const std::string& name,
+                               const OpencensusSpan* parent)
+    : span_(opencensus::trace::Span::StartSpan(name, parent ? &parent->span_: nullptr)) {}
+
+}  // namespace async_grpc
+
+#endif  // BUILD_TRACING
--- async_grpc/rpc_handler.h
+++ async_grpc/rpc_handler.h
@@ -21,11 +21,12 @@
 #include "async_grpc/rpc.h"
 #include "async_grpc/rpc_handler_interface.h"
 #include "async_grpc/rpc_service_method_traits.h"
-#include "google/protobuf/message.h"
+#include "async_grpc/span.h"
 #include "glog/logging.h"
+#include "google/protobuf/message.h"
 #include "grpc++/grpc++.h"
 #if BUILD_TRACING
-#include "opencensus/trace/span.h"
+#include "async_grpc/opencensus_span.h"
 #endif
 
 namespace async_grpc {
@@ -68,12 +69,12 @@ class RpcHandler : public RpcHandlerInterface {
 
 #if BUILD_TRACING
   RpcHandler()
-      : trace_span_(opencensus::trace::Span::StartSpan(
-            RpcServiceMethodConcept::MethodName())) {}
-  virtual ~RpcHandler() { trace_span_.End(); }
+      : span_(
+            OpencensusSpan::StartSpan(RpcServiceMethodConcept::MethodName())) {}
+  virtual ~RpcHandler() { span_->End(); }
 
   // TODO(cschuet): consider wrapping to remove opencensus from API.
-  opencensus::trace::Span* trace_span() { return &trace_span_; }
+  Span* trace_span() { return span_.get(); }
 #endif
 
   void SetExecutionContext(ExecutionContext* execution_context) override {
@@ -88,7 +89,7 @@ class RpcHandler : public RpcHandlerInterface {
   void Finish(::grpc::Status status) {
     rpc_->Finish(status);
 #if BUILD_TRACING
-    trace_span_.SetStatus((opencensus::trace::StatusCode)status.error_code());
+    span_->SetStatus(status);
 #endif
   }
   void Send(std::unique_ptr<ResponseType> response) {
@@ -107,9 +108,7 @@ class RpcHandler : public RpcHandlerInterface {
  private:
   Rpc* rpc_;
   ExecutionContext* execution_context_;
-#if BUILD_TRACING
-  opencensus::trace::Span trace_span_;
-#endif
+  std::unique_ptr<Span> span_;
 };
 
 }  // namespace async_grpc
